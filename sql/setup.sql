6f0bad54535fab3920db2ed64803d39767d2bdfd
Erich Sauer <erich.sauer@gmail.com>
2022-02-15 18:37:13 -0800

    express backend to react frontend

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS sparkles CASCADE;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   username TEXT NOT NULL,
   email TEXT,
   avatar TEXT
+);
+
+CREATE TABLE sparkles (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  text TEXT NOT NULL,
+  user_id BIGINT NOT NULL REFERENCES github_users(id)
 );
